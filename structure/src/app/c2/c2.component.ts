4da808f96e9d96eddcff3a5edc2843d8450648ac
Ivan Costa <ivan.felipe@payshopx.com.br>
2021-01-04 09:22:45 -0300

    feat: component, component inline
    Signed-off-by: Ivan Costa <ivan.felipe@payshopx.com.br>

---
+++ structure/src/app/c2/c2.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-c2',
+  template: 'c2 Funcionando',
+  styleUrls: ['./c2.component.css']
+})
+export class C2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
