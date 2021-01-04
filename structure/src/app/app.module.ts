eeae8293f8b578ad0b216a77f5446ab5e735f4b6
Ivan Costa <ivan.felipe@payshopx.com.br>
2021-01-04 15:36:06 -0300

    feat: component jogo da velha;
    Signed-off-by: Ivan Costa <ivan.felipe@payshopx.com.br>

--- structure/src/app/app.module.ts
+++ structure/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { C1Component } from './c1/c1.component';
 import { C2Component } from './c2/c2.component';
 import { CounterComponent } from './counter/counter.component';
+import { TicTacToeComponent } from './tic-tac-toe/tic-tac-toe.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     C1Component,
     C2Component,
-    CounterComponent
+    CounterComponent,
+    TicTacToeComponent
   ],
   imports: [
     BrowserModule
